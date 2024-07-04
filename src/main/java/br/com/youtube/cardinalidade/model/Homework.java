e9fb279b44e053c9d79f5de9e2609e9eb66f2173
Antonioafj <antonioafj.edu@gmail.com>
2024-07-04 13:20:18 -0400

    primeiro commit

---
+++ src/main/java/br/com/youtube/cardinalidade/model/Homework.java
@@ -0,0 +1,20 @@
+package br.com.youtube.cardinalidade.model;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_homework")
+public class Homework {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_student", referencedColumnName = "id")
+    private Student student;
+
+}
